62e3723313ce309bcf853673d58b03cc8c9db339
Rahul Adya <radya_be22@thapar.edu>
2025-07-23 02:10:43 +0530

    error resolved

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
-import BestSeller from '../components/BestSeller'
+
+import BestSeller from '../components/BestSeller';
 import OurPolicy from '../components/OurPolicy'
 import NewsLetterBox from '../components/NewsLetterBox'
 
