59790e3fb890b96120fa10e1a1e66d6482f64be3
Rahul Adya <radya_be22@thapar.edu>
2025-09-08 17:36:09 +0530

    removed p tag

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -15,9 +15,6 @@ const LatestCollection = () => {
         <div className='my-10'>
             <div className="text-center py-8 text-3xl">
                 <Title text1={"LATEST"} text2={"COLLECTIONS"}/>
-                <p className='w-3/4 m-auto  text-xs sm:text-sm md:text-base text-gray-600'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit dicta maiores ad quasi minus voluptas vel, autem eveniet harum molestias?
-                </p>
             </div>
         {/*Rendering products */}
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
